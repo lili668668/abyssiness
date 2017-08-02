0a34980fb6f08a56c749a6d899c1a4bb80982d20
Tzu-Yin Hong <lili668668@gmail.com>
2017-08-02 17:16:58 +0800

    Use Password type

--- Models/Member.cs
+++ Models/Member.cs
@@ -5,10 +5,14 @@ namespace abyssiness.Models
 {
     public class Member
     {
+        [Required]
         [Display(Name = "帳號")]
         public string account { get; set; }
+        [Required]
         [Display(Name = "密碼")]
+        [DataType(DataType.Password)]
         public string password { get; set; }
+        [Required]
         [Display(Name = "姓名")]
         public string name { get; set; }
         [Display(Name = "聯絡電話")]
