0f3f2d4b5c15c65b28e134455a0649139e4c53dc
Tzu-Yin Hong <lili668668@gmail.com>
2017-08-02 15:55:09 +0800

    use "AddMember" name replace "Register" name

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using abyssiness.Models;
 
 namespace abyssiness.Controllers
 {
     public class RegisterController : Controller
     {
-        public IActionResult Register()
+        public IActionResult AddMember()
         {
             return View();
+        }
+
+        public async Task<IActionResult> NewMember([Bind("account, password, name, phone")] Member member)
+        {
+            Console.WriteLine("account:" + member.account);
+            Console.WriteLine("password:" + member.password);
+            Console.WriteLine("name:" + member.name);
+            Console.WriteLine("phone:" + member.phone);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
