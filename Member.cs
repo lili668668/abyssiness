25b32adcf0d63fa2ed4e40213dc0c3fd935b90c5
Tzu-Yin Hong <lili668668@gmail.com>
2017-08-02 18:03:45 +0800

    Try to add db connect

---
+++ Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace abyssiness
+{
+    public partial class Member
+    {
+        public Guid Id { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public TimeSpan CreateTime { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ abbyssContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace abyssiness
+{
+    public partial class abbyssContext : DbContext
+    {
+        public virtual DbSet<Member> Member { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseNpgsql(@"Host=localhost;Database=abbyss;Username=abbyss;Password=valley");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>(entity =>
+            {
+                entity.ToTable("member");
+
+                entity.HasIndex(e => e.Account)
+                    .HasName("member_account_key")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("uuid_generate_v4()");
+
+                entity.Property(e => e.Account)
+                    .IsRequired()
+                    .HasColumnName("account");
+
+                entity.Property(e => e.CreateTime)
+                    .HasColumnName("create_time")
+                    .HasColumnType("time")
+                    .HasDefaultValueSql("('now'::text)::time with time zone");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Phone).HasColumnName("phone");
+            });
+        }
+    }
+}
