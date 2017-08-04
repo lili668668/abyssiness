a9d8f67f5e48ea599fbd642c8cd1ffea9d2ffcfb
Tzu-Yin Hong <lili668668@gmail.com>
2017-08-04 16:46:28 +0800

    Unite the models

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using abyssiness.Models;
+using abyssiness;
 
 namespace abyssiness.Controllers
 {
@@ -17,10 +17,10 @@ namespace abyssiness.Controllers
         [HttpPost]
         public async Task<IActionResult> NewMember([Bind("account, password, name, phone")] Member member)
         {
-            Console.WriteLine("account:" + member.account);
-            Console.WriteLine("password:" + member.password);
-            Console.WriteLine("name:" + member.name);
-            Console.WriteLine("phone:" + member.phone);
+            Console.WriteLine("account:" + member.Account);
+            Console.WriteLine("password:" + member.Password);
+            Console.WriteLine("name:" + member.Name);
+            Console.WriteLine("phone:" + member.Phone);
             return RedirectToAction("Index", "Home");
         }
     }
--- Member.cs
+++ Member.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace abyssiness
 {
     public partial class Member
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "帳號")]
         public string Account { get; set; }
+
+        [Required]
+        [Display(Name = "密碼")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        
         public TimeSpan CreateTime { get; set; }
+
+        [Required]
+        [Display(Name = "姓名")]
         public string Name { get; set; }
+
+        [Display(Name = "聯絡電話")]
         public string Phone { get; set; }
     }
 }
