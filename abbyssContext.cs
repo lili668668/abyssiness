0cc788a533e11f92df78e1833d930fc351a51629
Tzu-Yin Hong <lili668668@gmail.com>
2017-08-22 16:45:22 +0800

    修正我依開始刪掉的錯誤
    喔喔喔喔喔喔喔喔喔，成功拉拉拉拉拉

--- abbyssContext.cs
+++ abbyssContext.cs
@@ -8,39 +8,44 @@ namespace abyssiness
     {
         public virtual DbSet<Member> Member { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Host=localhost;Database=abbyss;Username=abbyss;Password=valley");
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Member>(entity =>
-            {
-                entity.ToTable("member");
+                    {
+                    entity.ToTable("member");
 
-                entity.HasIndex(e => e.Account)
+                    entity.HasIndex(e => e.Account)
                     .HasName("member_account_key")
                     .IsUnique();
 
-                entity.Property(e => e.Id)
+                    entity.Property(e => e.Id)
                     .HasColumnName("id")
                     .HasDefaultValueSql("uuid_generate_v4()");
 
-                entity.Property(e => e.Account)
+                    entity.Property(e => e.Account)
                     .IsRequired()
                     .HasColumnName("account");
 
-                entity.Property(e => e.CreateTime)
+                    entity.Property(e => e.CreateTime)
                     .HasColumnName("create_time")
                     .HasColumnType("time")
                     .HasDefaultValueSql("('now'::text)::time with time zone");
 
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasColumnName("name");
+                    entity.Property(e => e.Name)
+                        .IsRequired()
+                        .HasColumnName("name");
 
-                entity.Property(e => e.Password)
-                    .IsRequired()
-                    .HasColumnName("password");
+                    entity.Property(e => e.Password)
+                        .IsRequired()
+                        .HasColumnName("password");
 
-                entity.Property(e => e.Phone).HasColumnName("phone");
-            });
+                    entity.Property(e => e.Phone).HasColumnName("phone");
+                    });
         }
     }
 }
